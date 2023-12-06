edfbccbf49cfd68fb07a5b77f4b1d9556e6d69c3
kanchanshinde93 <98524212+kanchanshinde93@users.noreply.github.com>
2023-12-06 17:36:46 +0530

    add css

--- src/app/main/dashboard/account/account.component.ts
+++ src/app/main/dashboard/account/account.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { OutletServiceService } from 'app/services/outlet-service.service';
-
+import { BlockUI, NgBlockUI } from "ng-block-ui";
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent implements OnInit {
+  @BlockUI() blockUI: NgBlockUI;
   sellerData:any
   constructor(private outletService: OutletServiceService) { }
 
@@ -15,7 +16,9 @@ export class AccountComponent implements OnInit {
   }
 
   sellInfo() {
+    this.blockUI.start();
     this.outletService.getSellerInfo().subscribe((data: any) => {
+      this.blockUI.stop();
       this.sellerData = data.items;
     });
   }
